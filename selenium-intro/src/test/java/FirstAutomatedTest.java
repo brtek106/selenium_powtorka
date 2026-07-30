10d883a6751696685c7cb48408d5b1a3d5731d8a
Bartek <bartlomiej.dudek94@gmail.com>
2026-07-30 22:12:20 +0200

    Zadanie 3.1

--- selenium-intro/src/test/java/FirstAutomatedTest.java
+++ selenium-intro/src/test/java/FirstAutomatedTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class FirstAutomatedTest {
@@ -26,6 +27,15 @@ public class FirstAutomatedTest {
         String pageTitle = driver.getTitle();
 
         assertTrue(pageTitle.contains("DuckDuckGo"));
+    }
+
+    @Test
+    public void mySecondTest() {
+        driver.navigate().to("https://selenium.dev/");
+
+        String pageTitle = driver.getTitle();
+
+        assertTrue(pageTitle.equals("Selenium"));
     }
 
     @AfterMethod
