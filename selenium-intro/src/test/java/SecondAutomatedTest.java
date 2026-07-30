8035a4310221dead3364f1671c791307a7f23ff7
Bartek <bartlomiej.dudek94@gmail.com>
2026-07-30 22:18:16 +0200

    Zadanie 3.2

--- selenium-intro/src/test/java/SecondAutomatedTest.java
+++ selenium-intro/src/test/java/SecondAutomatedTest.java
@@ -1,14 +1,12 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class FirstAutomatedTest {
+public class SecondAutomatedTest {
 
     private WebDriver driver;
 
@@ -18,15 +16,12 @@ public class FirstAutomatedTest {
     }
 
     @Test
-    public void myFirstTest() {
-        driver.navigate().to("https://duckduckgo.com/");
-
-        driver.findElement(By.name("q")).sendKeys("JavaStart");
-        driver.findElement(By.name("q")).submit();
+    public void mySecondTest() {
+        driver.navigate().to("https://selenium.dev/");
 
         String pageTitle = driver.getTitle();
 
-        assertTrue(pageTitle.contains("DuckDuckGo"));
+        assertTrue(pageTitle.equals("Selenium"));
     }
 
     @AfterMethod
