18cfd7d86d5d6679c77f1b4cec7d098dfa134e6d
Bartek <bartlomiej.dudek94@gmail.com>
2026-08-12 16:55:26 +0200

    Zadanie 8.3

--- selenium-intro/src/test/java/BaseTest.java
+++ selenium-intro/src/test/java/BaseTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
     public WebDriver driver;
-    public String baseUrl = "https://theinternet.przyklady.javastart.pl/dynamic_controls";
+    public String baseUrl = "https://theinternet.przyklady.javastart.pl/dynamic_loading/1";
 
     @BeforeMethod
     public void beforeTest() {
---
+++ selenium-intro/src/test/java/FluentWaitTests.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class FluentWaitTests extends BaseTest {
+
+    @Test
+    public void fluentWaitTest() {
+        WebElement helloWorldMessage = driver.findElement(By.cssSelector("#finish h4"));
+        assertFalse(helloWorldMessage.isDisplayed());
+
+        WebElement startButton = driver.findElement(By.cssSelector("#start > button"));
+        startButton.click();
+
+        FluentWait<WebDriver> fluentWait = new FluentWait<>(driver);
+        fluentWait
+                .pollingEvery(Duration.ofMillis(250))
+                .withTimeout(Duration.ofSeconds(15))
+                .until(ExpectedConditions.visibilityOf(helloWorldMessage));
+
+        assertTrue(helloWorldMessage.isDisplayed());
+    }
+}
